b50df183223e348ec53973df1bfcf43af499a65f
Lorenz Verzosa <lverzosa@users.noreply.github.com>
2019-02-04 19:06:15 -0800

    Update: README.md etl

--- etl/WordCount.scala
+++ etl/WordCount.scala
@@ -5,11 +5,11 @@
 
 // TODO change to read from dbfs
 
-val text = "This self-paced guide is the “Hello World” tutorial for Apache Spark using Databricks. In the following tutorial modules, you will learn the basics of creating Spark jobs, loading data, and working with data. You’ll also get an introduction to running machine learning algorithms and working with streaming data. Databricks lets you start writing Spark queries instantly so you can focus on your data problems."
+val text:String = "This self-paced guide is the “Hello World” tutorial for Apache Spark using Databricks. In the following tutorial modules, you will learn the basics of creating Spark jobs, loading data, and working with data. You’ll also get an introduction to running machine learning algorithms and working with streaming data. Databricks lets you start writing Spark queries instantly so you can focus on your data problems."
 
 // COMMAND ----------
 
-val rdd = sc.parallelize(Seq(text))
+val rdd = sc.parallelize(Seq(text.replaceAll("[^A-Za-z0-9 _]", "").toLowerCase))
 
 // COMMAND ----------
 
@@ -17,7 +17,11 @@ val tokenizedRdd = rdd.flatMap(_.split(" "))
 
 // COMMAND ----------
 
-tokenizedRdd.count()
+val distinctSum = tokenizedRdd.map( x => (x, 1) ).reduceByKey( _ + _ )
+
+// COMMAND ----------
+
+distinctSum.collect()
 
 // COMMAND ----------
 
